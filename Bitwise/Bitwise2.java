04b58566e6880bbc2fbbc24e859cd4c3dda48628
Jithu <jithukrishnaju@gmail.com>
2023-08-18 22:21:51 +0530

    problemset

--- Bitwise/Bitwise2.java
+++ Bitwise/Bitwise2.java
@@ -1,17 +1,17 @@
+//set the k th bit to 1 (if its 1 remains it one if its zero make it one.)
+
 import java.util.Scanner;
 
-//Here set find the k th bit.
-public class Bitwise1 {
+public class Bitwise2 {
   public static void main(String[] args) {
     Scanner in = new Scanner(System.in);
     int n = in.nextInt();
     int k = in.nextInt();
     int ans = getAns(n, k);
     System.out.println(ans);
-    in.close();
   }
 
   static int getAns(int n, int k) {
-    return n & (1 << (k - 1));
+    return n | (1 << (k - 1));
   }
 }
